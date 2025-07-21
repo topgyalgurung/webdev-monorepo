b41b99b7ebd7bf5db81113b32cea0525010ab60c
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-07-21 19:30:06 -0400

    node simple routing setup

--- node-js/html-to-browser/server.js
+++ node-js/html-to-browser/server.js
@@ -8,14 +8,29 @@ const server = http.createServer((req,res) => {
     // set header content type
     res.setHeader('Content-Type', 'text/html');
 
+    // simple routing system
+    let path = "./views/";
+    switch(req.url){
+        case '/':
+            path += 'index.html';
+            break;
+        case'/about':
+            path += 'about.html'
+            break;
+        default:
+            path += '404.html';
+            break;
 
-    fs.readFile('./views/index.html', (err, data) =>{
+    }
+
+    // fs.readFile('./views/index.html', (err, data) =>{
+    fs.readFile(path, (err, data) =>{
         if(err){
             console.log(err);
             res.end();
         }else{
-            // res.write(data);
-            res.end(); // if we send one thing we can send directly to end
+            // res.write(data); // write for multiple thing
+            res.end(data); // if we send one thing we can send directly to end
         }
     })
 })
