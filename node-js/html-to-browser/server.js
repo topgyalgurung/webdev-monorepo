51bf9e01a2d031ab367d019a2476470b429f8f8c
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-07-21 20:14:21 -0400

    npm

--- node-js/html-to-browser/server.js
+++ node-js/html-to-browser/server.js
@@ -1,9 +1,20 @@
 const http = require('http');
 const fs = require('fs');
+const _ = require('lodash');
+
+// node send tml to browser
+// basic routing 
+// status code
+// redirect 
+
+// status code: 200 -ok, 301-resource moved, 404 not found, 500 - internal server error
 
 const server = http.createServer((req,res) => {
 
-    console.log(req.url, req.method);
+    // console.log(req.url, req.method);
+
+    const num = _.random(0,20);
+    console.log(num);
     
     // set header content type
     res.setHeader('Content-Type', 'text/html');
@@ -13,14 +24,21 @@ const server = http.createServer((req,res) => {
     switch(req.url){
         case '/':
             path += 'index.html';
+            res.statusCode = 200;
             break;
         case'/about':
             path += 'about.html'
+            res.statusCode = 200;
+            break;
+        case '/about-me':
+            res.statusCode = 301;
+            res.setHeader('Location', '/about')
+            res.end();
             break;
         default:
             path += '404.html';
+            res.statusCode = 404;
             break;
-
     }
 
     // fs.readFile('./views/index.html', (err, data) =>{
