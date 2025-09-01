d03981ac3c1ee6b72fa72637b65a82c428e26fac
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-09-01 17:58:16 -0400

    moved state up

--- mern-stack/react/react-ts/react-ts-basics/src/App.tsx
+++ mern-stack/react/react-ts/react-ts-basics/src/App.tsx
@@ -1,12 +1,18 @@
 import Heading from "./components/Heading"
 import { Section } from "./components/Section"
 import Counter from "./components/Counter"
+
+import { useState } from "react";
+
+// passing state down 
 function App() {
+  const [ count, setCount] = useState(1); 
+
   return (
     <>
     <Heading title={"Hello"}/>
     <Section title = {"Different Title"}> This is my Section</Section>
-    <Counter/>
+    <Counter count = {count} setCount={setCount}/> 
     </>
   )
 }
--- mern-stack/react/react-ts/react-ts-basics/src/components/Counter.tsx
+++ mern-stack/react/react-ts/react-ts-basics/src/components/Counter.tsx
@@ -1,12 +1,20 @@
-import { useState } from "react"
-const Counter = () => {
-    const [ count, setCount] = useState(1); // explicit: <number>(1); if using null<number| null>(null); 
+// import { useState } from "react"
+
+// import type { ReactNode } from "react"
+
+type CounterProps = {
+    setCount: React.Dispatch<React.SetStateAction<number>>, // copy from App [setCount]
+    count: number
+    // count: ReactNode
+}
+const Counter = ({setCount, count}: CounterProps) => {
+    // const [ count, setCount] = useState(1); // explicit: <number>(1); if using null<number| null>(null); 
   return (
     <>
+    {/* <h1> Count is {count}</h1> */}
     <h1> Count is {count}</h1>
     <button onClick = { () => setCount(prev => prev + 1)}> + </button>
     <button onClick = { () => setCount(prev => prev - 1)}> - </button>
-
     </>
   )
 }
