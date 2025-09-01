700bcf814e2b3a2569be9b1d400afbfc755797f2
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-08-31 23:57:18 -0400

    react-ts

--- mern-stack/node-js/node_to_express.js
+++ mern-stack/node-js/node_to_express.js
@@ -4,6 +4,7 @@ import http from 'http'
 const server = http.createServer();
 const gets = []
 const posts = []
+
 // server.get('/test', (req,res) =>(test));
 
 server.on("request", (req,res) => {
@@ -21,10 +22,10 @@ server.get = f => gets.push(f)
 server.post = f => posts.push(f)
 
 // test get $curl -v http://[::1]:8080 // fancy same localhost
-server.get((req,res)=>res.write('Received your GET1'));
-server.get((req,res)=>res.end('Received your GET2'));
+server.get((req,res)=>res.write('Received your GET1\n'));
+server.get((req,res)=>res.end('Received your GET2\n'));
 // test post $curl -X POST http://localhost:8080
-server.post((req,res)=>res.write('Received your POST1'));
-server.post((req,res)=>res.end('Received your POST2'));
+server.post((req,res)=>res.write('Received your POST1\n'));
+server.post((req,res)=>res.end('Received your POST2\n'));
 
 server.listen(8080);
