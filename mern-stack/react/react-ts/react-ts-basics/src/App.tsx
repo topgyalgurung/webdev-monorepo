ff92a580409b19be33a01384a00aa8ef6a2fe613
Topgyal Gurung <topgyaltsering3@gmail.com>
2025-09-01 18:18:11 -0400

    create generic component

--- mern-stack/react/react-ts/react-ts-basics/src/App.tsx
+++ mern-stack/react/react-ts/react-ts-basics/src/App.tsx
@@ -1,6 +1,7 @@
 import Heading from "./components/Heading"
 import { Section } from "./components/Section"
 import Counter from "./components/Counter"
+import List from "./components/List";
 
 import { useState } from "react";
 
@@ -13,6 +14,9 @@ function App() {
     <Heading title={"Hello"}/>
     <Section title = {"Different Title"}> This is my Section</Section>
     <Counter count = {count} setCount={setCount}/> 
+    <List items = {["Coffee", "Tacos", "Code"]} render = {(item:
+    string) => <span className="gold">{item}</span>
+    }/>
     </>
   )
 }
---
+++ mern-stack/react/react-ts/react-ts-basics/src/components/List.tsx
@@ -0,0 +1,24 @@
+import type { ReactNode } from "react";
+
+//generic
+interface ListProps<T>{
+    items: T[], // array of items
+    render: (item: T) => ReactNode // METHOD
+}
+
+// TS hard time recognizing this generic
+//  solution: <T extends {}> or easier <T,>
+// or achieve same with function list
+const List = <T,> ({items, render}: ListProps<T>) => {
+  return (
+    <ul>
+        {items.map((item, i) => (
+            <li key={i}>
+                {render(item)}
+            </li>
+        ))}
+    </ul>
+  )
+}
+
+export default List
